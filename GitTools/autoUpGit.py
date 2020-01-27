e4933168c25ebf44fb92ab4694b7096c5d752978
Mike <60325499+leguim-repo@users.noreply.github.com>
2020-01-27 10:19:42 +0100

    Create autoUpGit.py

---
+++ GitTools/autoUpGit.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3.6
+# coding=utf-8
+
+import os
+
+
+if __name__ == '__main__':
+	#os.chdir(path)
+	#StartFolder = os.path.dirname(os.path.realpath(__file__))
+	# Directorio base del repositorio
+	StartFolder = '/Users/mike/Desktop/repositories/'
+	print(StartFolder)
+	ListOfFolders = next(os.walk('.'))[1]
+	for folder in ListOfFolders:
+		if (folder != 'gitlab_template'):
+			if os.path.exists(folder+"/UpGit.sh"):
+				print(folder+"\t->\tUpGit.sh\tExecuting UpGit.sh")
+				os.chdir(folder)
+				os.system('./UpGit.sh')
+				os.chdir(StartFolder)
+			else:
+				print(folder)
+			print("-"*30)
